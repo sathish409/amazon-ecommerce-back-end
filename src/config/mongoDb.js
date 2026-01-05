ee77834acc15ce6ce7432b03131aba8a94b99af8
sathish409 <sathish.boga409@gmail.com>
2026-01-05 22:58:39 +1100

    one last

--- src/config/mongoDb.js
+++ src/config/mongoDb.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
-
+const host = process.env.MONGO_URL.split("@")[1];
 export const connectDb = async () => {
   try {
     const con = await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    con && console.log("db is connected");
+    con && console.log(`db is connected running at ${host}`);
   } catch (error) {
     console.log(error);
   }
